058702b69d76268e78bd87e47748178a23e0d901
cmp3652 <79061058+cmp3652@users.noreply.github.com>
2024-01-07 16:35:27 -0500

    Add files via upload

---
+++ Numerical_Linear/SVD_Rover.m
@@ -0,0 +1,29 @@
+% Clara Pitkins
+% cmp3652@rit.edu
+% Numerical Linear Algebra Project
+% 5/2023
+
+% read the image
+rover = imread('Rover2.tiff');
+rover = double(rover);
+rover = rover(:,:,1);
+
+% Display image with scaled colors
+imagesc(rover);
+colormap(gray);
+
+% do SVD
+[U, S, V] = svd(rover);
+%display rank
+r = rank(rover)
+
+% number of modes
+m = 486;
+n = 608;
+p = 243;
+figure(1);
+rover1 = U(:,1:p)*S(1:p,1:p)*V(:,1:p)';
+imagesc(rover1);
+colormap(gray);
+title(['p = ', num2str(p)])
+ratio = (p+p*m+p*n)/(r+r*n+r*m)
